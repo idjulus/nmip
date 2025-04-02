b8c1e489ce4c1ef6f4656e99cd17d1f44b182f36
Dulus Ivanov <62487597+idjulus@users.noreply.github.com>
2025-04-02 17:21:15 +0900

    Update 4_Лекция3.py

--- lectures/4_Лекция3.py
+++ lectures/4_Лекция3.py
@@ -13,6 +13,13 @@ if menu == 'Метод регуляризации Тихонова':
 
 	"""
 
+	with open("archive/InverseLecture3.pdf", "rb") as file:
+		btn = st.download_button(
+			label="скачать Лекцию3",
+			data=file,
+			file_name="InverseLecture3.pdf",
+		)
+
 	def displayPDF(file):
 		with open(file,"rb") as f:
 			base64_pdf = base64.b64encode(f.read()).decode('utf-8')
@@ -23,4 +30,4 @@ if menu == 'Метод регуляризации Тихонова':
 
 		st.markdown(pdf_display, unsafe_allow_html=True)
 
-	displayPDF('../Archive/Lecture3/InverseLecture3.pdf')
+	displayPDF('archive/InverseLecture3.pdf')
